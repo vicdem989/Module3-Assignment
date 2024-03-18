391520b15c10c06ada169d8b1fa643379c73cf1f
vicdem989 <vicdemhoy@hotmail.com>
2024-03-18 17:33:42 +0100

    using math.sqrt was okay :)

--- module3/functions.cs
+++ module3/functions.cs
@@ -10,12 +10,7 @@ public class Functions {
         return lengthInInches * mmToInchesRatio;
     }
 
-    public static double GetRoot(double numberToGetRoot) {
-
-
-        //Using built in functionaly such as Math library
-
-        
+    public static double GetRoot(double numberToGetRoot) {        
         return Math.Sqrt(numberToGetRoot); 
     }
 
@@ -34,15 +29,3 @@ public class Functions {
 }
 
 
-/*
-
-Create the following functions:
-
-A function that returns the square of a number
-A function that returns a length in mm assuming it has been given a length in inches.
-A function that returns the root of a number
-A function that returns the cube of a number
-A function that returns the area of a circle given the radius.
-A function that returns a greeting, given a name.
-
-*/
